e0b6e0f0c6893a01a23acc6a4c766483d7ab13ed
ArtemLizantan <plishkan25@gmail.com>
2024-10-28 12:33:08 +0200

    button

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,14 +1,82 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import { useRef, useState } from 'react';
+import { gsap } from 'gsap';
 
-const Button = ({ route, title, color = '#5c74f1', background = ' #fff' }) => {
+const Button = ({ route, title, color = '#5c74f1', background = '#fff' }) => {
+  const hoverRef = useRef(null);
+  const buttonRef = useRef(null);
+  const [isActive, setIsActive] = useState(false); // Состояние для активного класса
+
+  const handleMouseEnter = () => {
+    setIsActive(true); // Устанавливаем активный класс
+
+    gsap.fromTo(
+      hoverRef.current,
+      {
+        scaleY: 0,
+        scale: 0.9,
+        borderTopLeftRadius: '20vw',
+        borderTopRightRadius: '20vw',
+        transformOrigin: '50% bottom',
+      },
+      {
+        scaleY: 1,
+        scale: 1,
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+        duration: 0.4,
+        ease: 'power2.out',
+      }
+    );
+
+    gsap.to(buttonRef.current, {
+      scale: 0.5,
+      duration: 0.1,
+      ease: 'power2.out',
+      onComplete: () => {
+        gsap.to(buttonRef.current, {
+          scale: 1,
+          duration: 0.1,
+          ease: 'power2.out',
+        });
+      },
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setIsActive(false); // Убираем активный класс
+
+    gsap.to(hoverRef.current, {
+      scaleY: 0,
+      scale: 0.9,
+      borderTopLeftRadius: '20vw',
+      borderTopRightRadius: '20vw',
+      duration: 0.4,
+      ease: 'power2.out',
+    });
+
+    gsap.to(buttonRef.current, {
+      scale: 1,
+      duration: 0.3,
+      ease: 'power2.out',
+    });
+  };
+
   return (
     <Link
-      className="link"
-      style={{ color: color, background: background }}
+      className={`link ${isActive ? 'active' : ''}`} // Добавляем активный класс
+      style={{
+        color: color,
+        background: background,
+        overflow: 'hidden',
+      }}
       to={route}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      ref={buttonRef}
     >
       {title}
+      <div className="link__hover" ref={hoverRef} />
     </Link>
   );
 };
